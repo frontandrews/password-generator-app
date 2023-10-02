74cfa97608db6e75bb3ffd8e8b16a7192ec26e77
Front-Andrews <andrews.ribeiro.gomes@gmail.com>
2023-10-02 20:15:43 -0300

    fix: config to export app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: '/possword-generator-app',
+    output: "export",
+}
 
 module.exports = nextConfig
